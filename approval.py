62a17ded5187ce75466c6e60e0aae9efd70d6e30
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-05-30 20:47:18 +0530

    Update approval.py

--- approval.py
+++ approval.py
@@ -1,1 +1,10 @@
+import uvicorn
+import joblib
 
+import google.genai as genai
+
+from fastapi import FastAPI
+from fastapi import APIRouter
+
+from pydantic import BaseModel
+model=joblib.load("best_model.joblib")
