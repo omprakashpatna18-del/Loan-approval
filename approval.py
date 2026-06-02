cee059ca4e84658cbd83faa938ce7f2edc6bfc9b
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-02 13:54:47 +0530

    Update approval.py

--- approval.py
+++ approval.py
@@ -28,19 +28,19 @@ class Loan(BaseModel):
   bank_asset_value:float
   
 def helper(data:Loan):
-  raw={
-    "no_of_dependents":data.get("no_of_dependents"),
-    "loan_amount":data.get("loan_amount"),
-    "cibil_score":data.get("cibil_score"),
-  "education":str(data.get("education")).title().strip(),
-  "self_employed":str(data.get("self_employed")).title().strip(),
-  "loan_term":data.get("loan_term"),
-  "residential_assets_value":data.get("residential_assets_value"),
-  "commercial_assets_value":data.get("commercial_assets_value"),
-  "luxury_assets_value":data.get("luxury_assets_value"),
-  "bank_asset_value":data.get("bank_asset_value")
-                      }
-  return pd.DataFrame([raw])
+  raw = {
+        "no_of_dependents": data.no_of_dependents,
+        "loan_amount": data.loan_amount,
+        "cibil_score": data.cibil_score,
+        "education": data.education.title().strip(),
+        "self_employed": data.self_employed.title().strip(),
+        "loan_term": data.loan_term,
+        "residential_assets_value": data.residential_assets_value,
+        "commercial_assets_value": data.commercial_assets_value,
+        "luxury_assets_value": data.luxury_assets_value,
+        "bank_asset_value": data.bank_asset_value
+    }
+    return raw
 @app.post("/predict")
 def predict(data:Loan):
   model_df=helper(data)
